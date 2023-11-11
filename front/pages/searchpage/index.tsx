d65d5ea43fe9193765668ed8123c045fb74caee0
mael reynaud <mael.reynaud@epita.fr>
2023-11-11 16:09:36 +0100

    feat: template

--- front/pages/searchpage/index.tsx
+++ front/pages/searchpage/index.tsx
@@ -1,35 +1,82 @@
-import Image from "next/image";
 import AppBar from "../index"
-import App from "next/app";
-import Grid from "@mui/joy/Grid"
-import Box from "@mui/joy/Grid"
 import Button from "@mui/joy/Button"
-import { Divider } from "@mui/joy";
+import { Dispatch, SetStateAction, useState } from "react";
 
-function Search() {
+type MenuProps  = {
+  selectedMenu: number;
+  setSelectedMenu: Dispatch<SetStateAction<number>>;
+}
 
-  const wrapperStyle = {
-    borderRadius: 12, // Adjust the value to change the roundness
-    overflow: 'hidden', // Ensures children stay within the rounded corners
-  };
+function LeftMenu({selectedMenu, setSelectedMenu} : MenuProps) {
+  return (
+    <div className="settings">
+      <Button
+        className={selectedMenu === 0 ? "mainButtonSelected" : "mainButton"} onClick={
+          () => setSelectedMenu(0)
+        } variant="plain" size="lg">Personal quest</Button>
+      <div className="divider-menu"></div>
+      <Button className={selectedMenu === 1 ? "mainButtonSelected" : "mainButton"} onClick={
+        () => setSelectedMenu(1)
+      } variant="plain" size="lg">Forum</Button>
+      <div className="divider-menu"></div>
+      <Button className={selectedMenu === 2 ? "mainButtonSelected" : "mainButton"} onClick={
+        () => setSelectedMenu(2)
+      } variant="plain" size="lg">Communautary quest</Button>
+      <Button className="showMore" size="md" style={{ backgroundColor: 'transparent' }} >
+        Show more
+      </Button>
+    </div>
+  )
+}
+
+function Personal() {
+  return (
+    <div className="topics">
+      Personal quest
+    </div>
+  )
+}
+
+function Forum() {
+  return (
+    <div className="topics">
+      Forum
+    </div>
+  )
+}
+
+function Communautary() {
+  return (
+    <div className="topics">
+      Communautary quests
+    </div>
+  )
+}
+
+function RightMenu({selectedMenu, setSelectedMenu} : MenuProps) {
+  if (selectedMenu === 0) {
+    return <Personal />
+  } else if (selectedMenu === 1) {
+    return <Forum />
+  } else {
+    return <Communautary/>
+  }
+}
+
+type SearchProps = {
+  subject: string;
+}
+
+function Search({subject} : SearchProps) {
+  const [selectedMenu, setSelectedMenu] = useState<number>(0)
 
   return (
     <>
+      <h1 className="subject">{subject}</h1>
       <div className="container-p">
         <div className="container">
-          <div className="settings">
-              <Button className="mainButton" variant="plain" size="lg">Personal quest</Button>
-              <Divider />
-              <Button className="mainButton" variant="plain" size="lg">Forum</Button>
-              <Divider />
-              <Button className="mainButton" variant="plain" size="lg">Communautary quest</Button>
-              <Button className="showMore" size="sm" style={{ backgroundColor: 'transparent' }} >
-                Show more
-              </Button>
-          </div>
-          <div className="topics">
-            right side
-          </div>
+          <LeftMenu selectedMenu={selectedMenu} setSelectedMenu={setSelectedMenu}/>
+          <RightMenu selectedMenu={selectedMenu} setSelectedMenu={setSelectedMenu}/>
         </div>
       </div>
     </>
@@ -37,10 +84,12 @@ function Search() {
 }
 
 export default function SearchPage() {
+  const subject = "Cuisine" // NEED TO BE GET FROM URL PARAMETERS
+
   return (
     <>
       <AppBar />
-      <Search />
+      <Search subject={subject}/>
     </>
   );
 }
